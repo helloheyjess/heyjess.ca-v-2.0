0256583ac9744676f5df8d209720d4bdb990e0b7
helloheyjess <jessicamayblack@gmail.com>
2015-07-04 11:06:33 -0400

    Single Page Portfolio Site
    HackerYou Week 1 Project

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+$(function(){
+	// Smooth Scroll
+	$('a').smoothScroll({
+		speed: 800
+	});
+
+	// Sticky Navigation
+
+	$(window).scroll(function(){
+
+	  var scrolled = $(window).scrollTop(); 
+	  var mainTop = $('#mainNav').offset().top;
+
+	  if(scrolled > mainTop){
+	    $("ul.nav").addClass("sticky-nav");
+	    $('#about').css('padding-top' , '80px' );
+	  } else{
+	    $("ul.nav").removeClass("sticky-nav");
+	    $('#about').css('padding-top', '0');
+	  }
+
+	});
+
+});
